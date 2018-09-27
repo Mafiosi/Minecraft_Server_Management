4a461e2fc3cbba7c67c5e9b4da5554ce53a1ea42
Joao Beleza <joao.beleza7@gmail.com>
2018-09-27 23:44:14 +0100

    Final Version 3.3     - Easier Management

---
+++ Source_Code/Reset_World.py
@@ -0,0 +1,99 @@
+from wakeonlan import send_magic_packet
+from fabric2 import Connection
+import marshal
+import types
+
+import socket
+import time
+
+
+def main():
+
+    ##########################################
+    ##########      VARIABLES       ##########
+    ##########################################
+
+    s = open('configs.pyc', 'rb')
+    s.seek(12)
+    olives = marshal.load(s)
+
+    garden = types.ModuleType("Garden")
+    exec(olives,garden.__dict__)
+
+    u = garden.pick(1)
+    p = garden.pick(2)
+    d = garden.pick(3)
+    m = garden.pick(4)
+    po = 9
+
+    #CONNECTION VARIABLES
+    server = Connection(host=d, user=u, connect_kwargs={
+        "password": p})
+
+    #TIME PC TAKES TO TURN ON
+    zzz = 60
+
+    ##########################################
+    ##########     MAIN PROGRAM     ##########
+    ##########################################
+
+    while True:
+        print("Checking if PC is already ON")
+
+        #TRY CONNECTING TO PC
+        try:
+            server.run('ls',hide=True)
+        except:
+            print("PC is turned off\nTurning it ON...")
+
+        verify = server.is_connected
+        #CHECKS IF PC IS ALREADY ON
+        if verify:
+            print("PC is turned ON")
+            print("Initializing Process...")
+
+            #TRY TO DELETE WORLD FOLDER
+            try:
+                server.run("cd /usr/minecraft/server; rm -r -f /world; mkdir world ")
+                print("World Folder was deleted")
+                break
+            except:
+                print("Couldn't Delete World, Check Connection.")
+                break
+
+        #IF PC IS TURNED OFF
+        else:
+
+            print('Looking up server info')
+            try:
+                i = socket.gethostbyname(d)
+            except:
+                print("Server info could not be retrieved")
+                break
+
+            #TELLS PC TO TURN ON
+            print('Waking up PC')
+            try:
+                send_magic_packet(m, ip_address=i, port=po)
+            except:
+                print("PC cannot be turned ON")
+                break
+
+            print("Waiting for PC to turn ON. ETA: ~60 sec")
+            time.sleep(zzz)
+
+            #INITIALIZING MINECRAFT SERVER BY RUNNING SERVER MANAGER
+            print("Initializing Process...")
+
+            #TRY TO DELETE WORLD FOLDER
+            try:
+                server.run("cd /usr/minecraft/server; rm -r -f /world; mkdir world ")
+                print("World Folder was deleted")
+                break
+            except:
+                print("Couldn't Delete World, Check Connection.")
+                break
+
+if __name__ == '__main__':
+
+    main()
